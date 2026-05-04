7a3b632df7c8eca5b5a66fbce3065a7be9cf58f4
gilrifo <riverafonsecag700@gmail.com>
2026-05-04 13:00:15 -0500

    ajustes del widget

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,15 @@
 package io.ionic.starter;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // 🔥 Forzar actualización del widget
+        GameWidget.forceUpdate(getApplicationContext());
+    }
+}
